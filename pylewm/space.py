41f5cddc84577ebb4f4757c862e65ccbb5586191
Lucas de Vries <lucasdevries@gmail.com>
2020-01-06 13:30:25 +0100

    Add some locking to space methods since they are used from multiple threads.

--- pylewm/space.py
+++ pylewm/space.py
@@ -1,6 +1,7 @@
 from pylewm.layouts.sidebar import SidebarLayout
 from pylewm.layouts.columns import ColumnsLayout
 import traceback
+import threading
 
 class Space:
     Layouts = [
@@ -20,24 +21,28 @@ class Space:
         self.pending_drop_slot = None
         self.focus_mru = []
 
+        self.space_lock = threading.RLock()
         self.layout_index = 0
         self.layout = None
         self.switch_layout(0)
 
     def get_last_focus(self):
-        if self.focus_mru:
-            return self.focus_mru[-1]
+        with self.space_lock:
+            if self.focus_mru:
+                return self.focus_mru[-1]
         return None
 
     def show(self):
-        self.visible = True
-        for window in self.windows:
-            window.show()
+        with self.space_lock:
+            self.visible = True
+            for window in self.windows:
+                window.show()
 
     def hide(self):
-        self.visible = False
-        for window in self.windows:
-            window.hide()
+        with self.space_lock:
+            self.visible = False
+            for window in self.windows:
+                window.hide()
 
     def update_focus(self, focus_window):
         # Update standard focus tracking
@@ -63,68 +68,78 @@ class Space:
             self.focus_mru.append(self.focus)
 
     def update_layout(self, focus_window):
-        self.update_focus(focus_window)
+        with self.space_lock:
+            self.update_focus(focus_window)
 
-        self.layout.focus_mru = self.focus_mru
-        self.layout.pending_drop_slot = self.pending_drop_slot
-        self.layout.focus = self.focus
-        self.layout.rect.assign(self.rect)
-        self.layout.update_layout()
+            self.layout.focus_mru = self.focus_mru
+            self.layout.pending_drop_slot = self.pending_drop_slot
+            self.layout.focus = self.focus
+            self.layout.rect.assign(self.rect)
+            self.layout.update_layout()
 
     def add_window(self, window, at_slot=None, direction=None):
-        assert not window.space
+        with self.space_lock:
+            assert not window.space
 
-        window.space = self
-        self.windows.append(window)
-        self.layout.add_window(window, at_slot, direction)
-        self.focus_mru.insert(0, window)
+            window.space = self
+            self.windows.append(window)
+
+            self.layout.add_window(window, at_slot, direction)
+            self.focus_mru.insert(0, window)
 
     def remove_window(self, window):
-        assert window.space == self
+        with self.space_lock:
+            assert window.space == self
 
-        lost_index = self.windows.index(window)
-        self.windows.remove(window)
-        self.focus_mru.remove(window)
-        window.space = None
+            lost_index = self.windows.index(window)
+            self.windows.remove(window)
+            self.focus_mru.remove(window)
+            window.space = None
 
-        if self.focus is window:
-            self.focus = self.layout.get_focus_window_after_removing(window)
+            if self.focus is window:
+                self.focus = self.layout.get_focus_window_after_removing(window)
 
-        if self.last_focus is window:
-            self.last_focus = self.layout.get_focus_window_after_removing(window)
+            if self.last_focus is window:
+                self.last_focus = self.layout.get_focus_window_after_removing(window)
 
-        self.layout.remove_window(window)
+            self.layout.remove_window(window)
 
     def set_pending_drop_slot(self, slot):
-        self.pending_drop_slot = slot
+        with self.space_lock:
+            self.pending_drop_slot = slot
 
     def get_window_in_direction(self, from_window, direction):
-        return self.layout.get_window_in_direction(from_window, direction)
+        with self.space_lock:
+            return self.layout.get_window_in_direction(from_window, direction)
 
     def move_window_in_direction(self, window, direction):
-        return self.layout.move_window_in_direction(window, direction)
+        with self.space_lock:
+            return self.layout.move_window_in_direction(window, direction)
 
     def get_drop_slot(self, position, rect):
-        return self.layout.get_drop_slot(position, rect)
+        with self.space_lock:
+            return self.layout.get_drop_slot(position, rect)
 
     def get_focus_window_after_removing(self, window_before_remove):
-        return self.layout.get_focus_window_after_removing(window_before_remove)
+        with self.space_lock:
+            return self.layout.get_focus_window_after_removing(window_before_remove)
 
     def switch_layout(self, movement):
-        self.layout_index = (self.layout_index + movement + len(Space.Layouts)) % len(Space.Layouts)
+        with self.space_lock:
+            self.layout_index = (self.layout_index + movement + len(Space.Layouts)) % len(Space.Layouts)
 
-        old_layout = self.layout
-        self.layout = Space.Layouts[self.layout_index]()
-        self.layout.focus_mru = self.focus_mru
-        self.layout.rect.assign(self.rect)
+            old_layout = self.layout
+            self.layout = Space.Layouts[self.layout_index]()
+            self.layout.focus_mru = self.focus_mru
+            self.layout.rect.assign(self.rect)
 
-        handled = False
-        if old_layout:
-            handled = self.layout.takeover_from_layout(old_layout)
+            handled = False
+            if old_layout:
+                handled = self.layout.takeover_from_layout(old_layout)
+                if not handled:
+                    handled = self.layout.takeover_from_windows(self.focus_mru)
+
             if not handled:
-                handled = self.layout.takeover_from_windows(self.focus_mru)
-
-        if not handled:
-            for window in self.focus_mru:
-                drop_slot, force_drop = self.layout.get_drop_slot(window.rect.center, window.rect)
-                self.layout.add_window(window, at_slot=drop_slot)
+                for window in self.focus_mru:
+                    drop_slot, force_drop = self.layout.get_drop_slot(window.rect.center, window.rect)
+                    self.layout.add_window(window, at_slot=drop_slot)
